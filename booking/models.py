57f32a64e6bfab004767ae235f4c30f25bb426b9
DevFabrizio <rockout94@hotmail.it>
2023-06-20 13:36:12 +0000

    add superuser to access admin page, remove fields from models

--- booking/models.py
+++ booking/models.py
@@ -6,7 +6,6 @@ class Customer(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
-    customer_id = models.IntegerField(unique=True)
 
     def __str__(self):
         return self.first_name
@@ -15,11 +14,10 @@ class Customer(models.Model):
 class Booking(models.Model):
     time_of_res = models.DateTimeField(auto_now=False, auto_now_add=False)
     num_of_guests = models.IntegerField()
-    res_id = models.IntegerField(unique=True)
     kids_under_10 = models.BooleanField(default=False)
     allergies = models.CharField(max_length=200)
     customer_id = models.ForeignKey('Customer', on_delete=models.CASCADE)
     res_name = models.CharField(max_length=20)
 
     def __str__(self):
-        return self.res_name
+        return self.res_name, self.time_of_res
