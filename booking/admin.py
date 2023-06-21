b839ec980f2526c19d991d679284fb70a141b1c1
DevFabrizio <rockout94@hotmail.it>
2023-06-21 07:16:03 +0000

    add view, template for base.html

--- booking/admin.py
+++ booking/admin.py
@@ -5,11 +5,13 @@ from .models import Customer, Booking
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
     list_filter = ('reservation_name', 'time_of_reservation')
+    list_display = ('reservation_name', 'time_of_reservation', 'num_of_guests')
 
 
 @admin.register(Customer)
 class CustomerAdmin(admin.ModelAdmin):
     list_filter = ('first_name', 'last_name')
+    list_display = ('first_name', 'last_name')
 
 
 
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Booking, Customer
 
 # Create your views here.
+
+
+class Booking(generic.ListView):
+    model = Booking
+    template_name = 'base.html'
