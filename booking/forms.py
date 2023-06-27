316ff6f53c7f67b58aa1125bb52e48e3ec2d9223
DevFabrizio <rockout94@hotmail.it>
2023-06-27 06:32:47 +0000

    add date and time picker widget to the booking form

--- booking/forms.py
+++ booking/forms.py
@@ -1,9 +1,12 @@
 from django.forms import ModelForm
 from .models import Booking
+from django.forms.widgets import DateTimeInput
 
 
 class BookingForm(ModelForm):
     class Meta:
         model = Booking
         fields = '__all__'
-       
+        widgets = {
+            'time_of_reservation': DateTimeInput(attrs={'type': 'datetime-local'})
+        }
