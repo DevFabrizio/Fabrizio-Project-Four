bf1be89b536f2271c30373f938c559fd4b6e50e1
DevFabrizio <rockout94@hotmail.it>
2023-07-13 11:54:21 +0000

    add comments to admin.py in booking app

--- booking/admin.py
+++ booking/admin.py
@@ -4,12 +4,18 @@ from .models import Booking, UserReservation
 
 @admin.register(Booking)
 class BookingAdmin(admin.ModelAdmin):
+    """
+    class to show the bookings made by users without the webiste account
+    """
     list_filter = ('time_of_reservation',)
     list_display = ('reservation_name', 'time_of_reservation', 'num_of_guests')
 
 
 @admin.register(UserReservation)
 class UserReservationAdmin(admin.ModelAdmin):
+    """
+    class to show the bookings made by users with an account
+    """
     list_filter = ('user', 'time_of_reservation')
     list_display = ('reservation_name',
                     'time_of_reservation',
