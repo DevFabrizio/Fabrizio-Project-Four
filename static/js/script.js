1e35a04336ca9a55d0280bb9deb992896bed1982
DevFabrizio <rockout94@hotmail.it>
2023-07-24 10:29:52 +0000

    correct syntax for JS file

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // timeout function to display the reservation success message for 3 secs
 
 setTimeout(function(){
-    let successMsg = document.getElementById('reservation-success-msg')
-    successMsg.remove()
-},3000)
+    let successMsg = document.getElementById('reservation-success-msg');
+    successMsg.remove();
+},3000);
 
