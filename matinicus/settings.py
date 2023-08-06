9d4564843792afbd9a7f393d8aa4ee05e4740a8a
DevFabrizio <rockout94@hotmail.it>
2023-08-06 07:00:28 +0000

    add img for ERD in README

--- matinicus/settings.py
+++ matinicus/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '8000-devfabrizio-fabriziopro-7q3ycxov6ed.ws-eu100.gitpod.io',
