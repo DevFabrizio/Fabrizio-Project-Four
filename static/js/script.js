0fa9d82661baae6a9265a6a8c1778ac25d1b5899
DevFabrizio <rockout94@hotmail.it>
2023-08-06 07:34:01 +0000

    modify the JS file to run the function on dom content loaded

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,14 @@
 // timeout function to display the reservation success message for 3 secs
 
-setTimeout(function(){
-    let successMsg = document.getElementById('reservation-success-msg');
-    successMsg.remove();
-},3000);
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    setTimeout(function(){
+        let successMsg = document.getElementById('reservation-success-msg');
+        if (successMsg) {
+            successMsg.remove();
+        }
+    }, 3000);
+
+});
 
